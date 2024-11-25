65bbefb98810d7337af95c70682389fe9f64d021
Peter-Pan-outstanding <nihaozhoujunjie@gmail.com>
2024-11-25 13:21:53 +0800

    Initial commit

---
+++ src/main/java/wtc/carbon/carbonbackend/CarbonBackendApplication.java
@@ -0,0 +1,13 @@
+package wtc.carbon.carbonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarbonBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wtc/carbon/carbonbackend/CarbonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package wtc.carbon.carbonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
