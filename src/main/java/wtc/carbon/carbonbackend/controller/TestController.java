fbcee3396bfa6c39ea91021118d5670e21606b65
Peter-Pan-outstanding <nihaozhoujunjie@gmail.com>
2024-11-25 13:47:43 +0800

    创建了分支 main分支是主分支，功能分支（feature/*）、修复分支（fix/*）

--- src/main/java/wtc/carbon/carbonbackend/controller/TestController.java
+++ src/main/java/wtc/carbon/carbonbackend/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 @RestController
@@ -17,5 +18,13 @@ public class TestController {
         map.put("key1", "value1");
         map.put("key2", "value2");
         return map;
+    }
+
+    @GetMapping("/getList")
+    public ArrayList<String> getList() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("value1");
+        list.add("value2");
+        return list;
     }
 }
